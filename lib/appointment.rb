12c063d03d85a1a28f1dfe42e09ec7ca9d9bf32d
Amanda Rolon <abrolon87@gmail.com>
2020-01-10 07:01:30 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,16 @@
+def Appointment
+  @@all = []
+
+  attr_accessor :date, :patient, :doctor
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
